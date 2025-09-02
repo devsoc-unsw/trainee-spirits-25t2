1bdda4e0283b005842e724729ffb8b5180aa5762
Kohtaroh Yamanishi <kohtaroh881@gmail.com>
2025-09-03 00:18:01 +1000

    added viewable map

--- frontend/geomemo_frontend/src/components/Map.jsx
+++ frontend/geomemo_frontend/src/components/Map.jsx
@@ -1,3 +1,12 @@
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css"
+
 export default function Map() {
-  return <div>Map</div>;
+  return (
+    <MapContainer center={[48.8566, 2.3522]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+    </MapContainer>
+  );
 }
